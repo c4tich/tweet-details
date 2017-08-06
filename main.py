8b807517300b4a66d52e84f4ea7d009732ac0509
c4tich <christianfortich@hotmail.com>
2017-08-06 20:23:19 +0200

    Commit inicial

---
+++ main.py
@@ -0,0 +1,10 @@
+webAccess=True;
+
+if webAccess:
+	print('This program will provide a tweet\'s detail\n')
+	print('You will have to provide either: \n')
+	print('\t Its identifier (i.e. 18 char long number)\n')
+	print('\t Its URL (with or without https://')
+
+	tweetID=input()
+	print(tweetID)
